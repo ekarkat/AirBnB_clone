d16ce9567b20876f795a10f150b55eb6508f9e14
seroking <baridmohamedamine004@gmail.com>
2023-10-11 15:51:27 -0700

    more classes

--- console.py
+++ console.py
@@ -3,12 +3,14 @@
 
 
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """ cmd class """
 
     prompt = "(hbnb) "
+
+    cls = {"BaseModel", "User"}
 
     def do_quit(self, arg):
         """ Quit command to exist the program """
@@ -21,6 +23,41 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ skip empty line"""
         pass
+
+    def do_create(self, arg):
+        """ create a new instance of the class BaseModel """
+        if arg == None:
+            print ("** class doesn't exist **")
+        else:
+            try:
+                new_instance = eval(arg)()
+                new_instance.save()
+                print (new_instance.id)
+            except NameError:
+                print ("** class name missing **")
+
+
+    def do_show(self, arg):
+        """ show the string representaion of an instance based on ID """
+        args = arg.split()
+        if not args:
+            print ("** class name missing **")
+        else:
+            cls_name = args[0]
+            if len(args) == 0:
+                print("** class name missing **")
+            elif cls_name not in HBNBCommand.cls:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key_obj = "{}.{}".format(cls_name, args[1])
+		# a terminer apres la creation de la classe storage
+
+
+
+
+
 
 
 if __name__ == '__main__':
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Modul State Class"""
+
+
+from model.base_modle import BaseModel
+
+class State(BaseModel):
+    """
+    State class representations
+
+    args:
+        name : name of the state
+    """
+    name = ""
