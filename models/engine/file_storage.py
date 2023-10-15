3e21e8fa0d6dda98d5804f90b6dddabf04c6736f
seroking <baridmohamedamine004@gmail.com>
2023-10-15 07:26:19 -0700

    filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,27 +12,27 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return (FileStorage.__objects)
+        return (self.__objects)
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        FileStorage.__objects[str(obj.__class__.__name__ + "." + str(obj.id))] = obj
+        self.__objects[str(obj.__class__.__name__ + "." + str(obj.id))] = obj
 
     def save(self):
         obj_dic = {}
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in self.__objects.items():
             obj_dic[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as obj_saves:
+        with open(self.__file_path, 'w', encoding="utf-8") as obj_saves:
             json.dump(obj_dic, obj_saves, indent=4)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as obj_saves:
+            with open(self.__file_path, 'r', encoding="utf-8") as obj_saves:
                 data = json.load(obj_saves)
                 objects = {}
                 for key, value in data.items():
                     obj = eval(data[key]["__class__"])(**value)
                     objects[key] = obj
-            FileStorage.__objects = objects
+            self.__objects = objects
         except FileNotFoundError:
             return
